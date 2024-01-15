0812a1b193c030f612feca7cb2ab1aca0cf1c253
Andreas Tsarida <tsaridas@users.noreply.github.com>
2024-01-15 23:47:30 +0100

    make links magnets (#3)
    * only magnet version

--- jackett.js
+++ jackett.js
@@ -62,99 +62,106 @@ const search = (apiKey, query, cb, end) => {
 			end([]);
 		});
 
-		try {
-			await Promise.all(apiIndexers.map(async (indexer) => {
-				if (!(indexer && indexer.attributes && indexer.attributes.id)) {
-					tick();
-					return;
-				}
 
-				const url = config.jackett.host + 'api/v2.0/indexers/' + indexer.attributes.id + '/results/torznab/api?apikey=' + apiKey + encodeURI(searchQuery);
-				config.debug && console.log(`Searching indexer ${indexer.attributes.id} with url ${url}`);
-
-				const response = await new Promise((resolve) => {
-					needle.get(url, {
-						open_timeout: config.jackett.openTimeout,
-						read_timeout: config.jackett.readTimeout,
-						parse_response: false
-					}, (err, resp) => {
-						resolve({ err, resp });
-					});
-				});
-
-				if (response.err || !response.resp || !response.resp.body) {
-					console.error(`Error ${response.err} when calling ${indexer.attributes.id}.`);
-					tick();
-					return;
-				}
-
-				//config.debug && console.log(`Response from ${indexer.attributes.id} is ${response.resp.body}.`);
-				const tors = xmlJs.xml2js(response.resp.body);
-
-				if (tors.elements && tors.elements[0] && tors.elements[0].elements && tors.elements[0].elements[0] && tors.elements[0].elements[0].elements) {
-					const elements = tors.elements[0].elements[0].elements;
-					const tempResults = [];
-
-					elements.forEach(elem => {
-						if (elem.type == 'element' && elem.name == 'item' && elem.elements) {
-							const newObj = {};
-							const tempObj = {};
-
-							elem.elements.forEach(subElm => {
-								if (subElm.name == 'torznab:attr' && subElm.attributes && subElm.attributes.name && subElm.attributes.value)
-									tempObj[subElm.attributes.name] = subElm.attributes.value;
-								else if (subElm.elements && subElm.elements.length)
-									tempObj[subElm.name] = subElm.elements[0].text;
-							});
-
-							const ofInterest = ['title', 'link', 'magneturl'];
-
-
-							ofInterest.forEach(ofInterestElm => {
-								if (tempObj[ofInterestElm])
-									newObj[ofInterestElm] = tempObj[ofInterestElm];
-							});
-
-							if (!newObj.magneturl || (newObj.link && !newObj.link.startsWith("magnet:"))) {
-								return;
-							}
-
-							const toInt = ['seeders', 'peers', 'size', 'files'];
-
-							toInt.forEach(toIntElm => {
-								if (tempObj[toIntElm])
-									newObj[toIntElm] = parseInt(tempObj[toIntElm]);
-							});
-
-							if (newObj.seeders < config.minimumSeeds || newObj.size > config.maximumSize) {
-								return;
-							}
-
-							if (tempObj.pubDate)
-								newObj.jackettDate = new Date(tempObj.pubDate).getTime();
-
-							newObj.from = indexer.attributes.id;
-
-							newObj.extraTag = helper.extraTag(newObj.title, query.name);
-							if (newObj.seeders > maxSeeder.number) {
-								maxSeeder.number = newObj.seeders;
-								maxSeeder.indexer = indexer.attributes.id;
-								maxSeeder.obj = newObj;
-							}
-							tempResults.push(newObj);
-						}
-					});
-					countResults += tempResults.length;
-					countFinished++;
-					config.debug && console.log("Found " + countResults + " so far from overall " + apiIndexers.length + " indexers " + countFinished + " finished. MaxSeeder: " + maxSeeder.number + " from " + maxSeeder.indexer + " Object " + JSON.stringify(maxSeeder.obj, null, 2));
-					cb(tempResults);
-				}
+		await Promise.all(apiIndexers.map(async (indexer) => {
+			if (!(indexer && indexer.attributes && indexer.attributes.id)) {
 				tick();
-			}));
-		} catch (error) {
-			console.error('Error calling Jackett:', error);
+				return;
+			}
+
+			const url = config.jackett.host + 'api/v2.0/indexers/' + indexer.attributes.id + '/results/torznab/api?apikey=' + apiKey + encodeURI(searchQuery);
+			config.debug && console.log(`Searching indexer ${indexer.attributes.id} with url ${url}`);
+
+			const response = await new Promise((resolve) => {
+				needle.get(url, {
+					open_timeout: config.jackett.openTimeout,
+					read_timeout: config.jackett.readTimeout,
+					parse_response: false
+				}, (err, resp) => {
+					resolve({ err, resp });
+				});
+			});
+
+			if (response.err || !response.resp || !response.resp.body) {
+				console.error(`Error ${response.err} when calling ${indexer.attributes.id}.`);
+				tick();
+				return;
+			}
+
+			//config.debug && console.log(`Response from ${indexer.attributes.id} is ${response.resp.body}.`);
+			const tors = xmlJs.xml2js(response.resp.body);
+
+			if (tors.elements && tors.elements[0] && tors.elements[0].elements && tors.elements[0].elements[0] && tors.elements[0].elements[0].elements) {
+				const elements = tors.elements[0].elements[0].elements;
+				const tempResults = [];
+
+				elements.forEach(elem => {
+					if (elem.type == 'element' && elem.name == 'item' && elem.elements) {
+						const newObj = {};
+						const tempObj = {};
+
+						elem.elements.forEach(subElm => {
+							if (subElm.name == 'torznab:attr' && subElm.attributes && subElm.attributes.name && subElm.attributes.value)
+								tempObj[subElm.attributes.name] = subElm.attributes.value;
+							else if (subElm.elements && subElm.elements.length)
+								tempObj[subElm.name] = subElm.elements[0].text;
+						});
+
+						const ofInterest = ['title', 'link', 'magneturl'];
+
+
+						ofInterest.forEach(ofInterestElm => {
+							if (tempObj[ofInterestElm])
+								newObj[ofInterestElm] = tempObj[ofInterestElm];
+						});
+
+						if (newObj.magneturl && newObj.magneturl.startsWith("magnet:") && (newObj.link && newObj.link.startsWith("http://"))) {
+							config.debug && console.log("Found magneturl " + newObj.magneturl + " and link " + newObj.link);
+							newObj.link = newObj.magneturl;
+						}
+
+						if (newObj.link && newObj.link.startsWith("magnet:") && ! newObj.magneturl) {
+							config.debug && console.log("Found missing magneturl: " + newObj.link);
+							newObj.magneturl = newObj.link;
+						}
+
+						if (!newObj.magneturl || (newObj.link && !newObj.link.startsWith("magnet:"))) {
+							return;
+						}
+
+						const toInt = ['seeders', 'peers', 'size', 'files'];
+
+						toInt.forEach(toIntElm => {
+							if (tempObj[toIntElm])
+								newObj[toIntElm] = parseInt(tempObj[toIntElm]);
+						});
+
+						if (newObj.seeders < config.minimumSeeds || newObj.size > config.maximumSize) {
+							return;
+						}
+
+						if (tempObj.pubDate)
+							newObj.jackettDate = new Date(tempObj.pubDate).getTime();
+
+						newObj.from = indexer.attributes.id;
+
+						newObj.extraTag = helper.extraTag(newObj.title, query.name);
+
+						if (newObj.seeders > maxSeeder.number) {
+							maxSeeder.number = newObj.seeders;
+							maxSeeder.indexer = indexer.attributes.id;
+							maxSeeder.obj = newObj;
+						}
+						tempResults.push(newObj);
+					}
+				});
+				countResults += tempResults.length;
+				countFinished++;
+				config.debug && console.log("Found " + countResults + " so far from overall " + apiIndexers.length + " indexers " + countFinished + " finished. MaxSeeder: " + maxSeeder.number + " from " + maxSeeder.indexer + " Object " + JSON.stringify(maxSeeder.obj, null, 2));
+				cb(tempResults);
+			}
 			tick();
-		}
+		}));
 	});
 };
 
