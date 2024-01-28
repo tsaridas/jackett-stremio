b8d3753c0e7d4a7c48132639ef86a3060aff1f15
Andreas Tsaridas <andreas.tsaridas@gmail.com>
2024-01-28 15:38:21 +0100

    icon

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const manifest = {
     "name": config.addonName,
     "description": "Stremio Add-on to get torrent results from Jackett",
 
-    "icon": "https://static1.squarespace.com/static/55c17e7ae4b08ccd27be814e/t/599b81c32994ca8ff6c1cd37/1508813048508/Jackett-logo-2.jpg",
+    "icon": "https://images.squarespace-cdn.com/content/55c17e7ae4b08ccd27be814e/1503363523826-E9M5KDF7RCQ805Y7TQ6H/Jackett-logo-2.jpg",
 
     // set what type of resources we will return
     "resources": [
