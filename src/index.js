4341866b8a3bc32e13c3f62fec390060e4d41396
Andreas Tsaridas <andreas.tsaridas@gmail.com>
2024-01-28 06:15:49 +0100

    remove ip

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ const manifest = {
 };
 
 addon.get('/manifest.json', (req, res) => {
-    console.log("Sending manifest to .", req.ip);
+    console.log("Sending manifest.");
     respond(res, manifest);
 });
 
@@ -312,7 +312,7 @@ addon.get('/stream/:type/:id.json', async (req, res) => {
         });
     }
 
-    console.log(`Q: ip: ${req.ip} / imdbiID: ${streamInfo.imdbId} / title: ${streamInfo.name} / type: ${streamInfo.type} / year: ${streamInfo.year}` +
+    console.log(`Q: imdbiID: ${streamInfo.imdbId} / title: ${streamInfo.name} / type: ${streamInfo.type} / year: ${streamInfo.year}` +
         (streamInfo.season && streamInfo.episode ? ` / season: ${streamInfo.season} / episode: ${streamInfo.episode}` : '') +
         '.');
 
@@ -333,7 +333,7 @@ addon.get('/stream/:type/:id.json', async (req, res) => {
             clearInterval(intervalId);
             const finalData = processTorrentList(streams);
             config.debug && console.log("Sliced & Sorted data ", finalData);
-            console.log(`A ip: ${req.ip} / imdbiID: ${streamInfo.imdbId} / Results ${finalData.length} / Timeout: ${(elapsedTime >= config.responseTimeout)} / Search Finished: ${searchFinished} / Queue Idle: ${asyncQueue.idle()} / Pending Downloads : ${inProgressCount} / Discarded : ${(streams.length - finalData.length)}`);
+            console.log(`A: imdbiID: ${streamInfo.imdbId} / Results ${finalData.length} / Timeout: ${(elapsedTime >= config.responseTimeout)} / Search Finished: ${searchFinished} / Queue Idle: ${asyncQueue.idle()} / Pending Downloads : ${inProgressCount} / Discarded : ${(streams.length - finalData.length)}`);
             return respond(res, {
                 streams: finalData,
                 "cacheMaxAge": 1440,
@@ -341,7 +341,7 @@ addon.get('/stream/:type/:id.json', async (req, res) => {
                 "staleError": 10080
             });
         }
-        config.debug && console.log("S / imdbiID: " + streamInfo.imdbId + " / Time Pending: " + (config.responseTimeout - elapsedTime) + " / Search Finished: " + searchFinished + " / Queue Idle: " + asyncQueue.idle() + " / Pending Downloads : " + inProgressCount + " / Processed Streams : " + streams.length);
+        config.debug && console.log(`S: imdbiID: ${streamInfo.imdbId} / Time Pending: ${(config.responseTimeout - elapsedTime)} / Search Finished: ${searchFinished} / Queue Idle: ${asyncQueue.idle()} / Pending Downloads : ${inProgressCount} / Processed Streams : ${streams.length}`);
 
     }, config.interval);
 
