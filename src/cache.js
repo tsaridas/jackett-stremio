851046ca30b53fcb502df8f45eecbc3aa95efddc
Andreas Tsarida <tsaridas@users.noreply.github.com>
2024-02-29 10:59:24 +0100

    cache (#56)

--- src/cache.js
+++ src/cache.js
@@ -7,16 +7,31 @@ function setCacheVariable(key, value, expirationMinutes) {
     };
 }
 
-function getCacheVariable(key) {
+function getCacheVariable(key, renewalMinutes = 0) {
     const cachedItem = cache[key];
 
     if (cachedItem && Date.now() < cachedItem.expirationTime) {
+        // Renew the expiration time
+        if (renewalMinutes > 0) {
+            cachedItem.expirationTime = Date.now() + renewalMinutes * 60 * 1000;
+        }
         return cachedItem.value;
     }
-
-    // Return null if the cache has expired or the key doesn't exist
     return null;
 }
+
+function cleanupExpiredItems() {
+    const currentTime = Date.now();
+    for (const key in cache) {
+        const cachedItem = cache[key];
+        if (currentTime >= cachedItem.expirationTime) {
+            delete cache[key];
+        }
+    }
+}
+
+// Call the cleanup function periodically, every 60 minutes
+setInterval(cleanupExpiredItems, 60 * 60 * 1000);
 
 module.exports = {
     setCacheVariable,
